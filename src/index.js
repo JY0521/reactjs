16f0460f67737d79f4d9cf0c20861b5dcb06b797
JY0521 <4792599@naver.com>
2023-06-09 13:43:09 +0900

    0609 스타일.모듈

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// import "./styles.css"; 버튼 디자인을 여러개 적용하고 싶으면 여기 import하면 안됨.
+
+// 스텝1. index.js에는 root에 렌더할 state들(App.js)를 작성하거나(물론 import하고 밑에추가)
+// index 에 글로벌하게 적용할 css를 임포트하거나 한다.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
