1339d94d2d5c053573cf07b630ce12978df89834
JY0521 <4792599@naver.com>
2023-06-09 13:43:29 +0900

    0609 스타일 모듈

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+// 스텝2. App.js에서는 state들을 만들어내는 곳이라고 생각하면 된다.
+
 function App() {
   return (
     <div>
-      <h1>돌아온 걸 환영해요!!! </h1>
+      <h1 className={styles.title}>돌아온 걸 환영해요!!! </h1>
+      <Button text={"컨티뉴!"} />
     </div>
   );
 }
