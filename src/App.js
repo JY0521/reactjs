3aece73188440b1ade35cb4ffb2e9bd7475673a5
JY0521 <4792599@naver.com>
2023-06-13 15:51:59 +0900

    0613 영화

--- src/App.js
+++ src/App.js
@@ -1,57 +1,58 @@
 import { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  //받아온 코인을 컴포넌트에 넣으려면 받아온 데이터를 useState에 넣어준다.
-  const [coins, setCoins] = useState([]);
-  //기본값을 []빈 배열로 두는 이유는 []빈 배열이 없으면
-  //코인데이터를 불러오기 전 coins의 값이 undifine상태이기 때문에 에러가 나게 됨.
-  const [myUSD, setMyUSD] = useState(0);
-  const [cost, setCost] = useState(1);
-  const onChange = (e) => {
-    setCost(e.target.value);
-    setMyUSD(1);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    //await로 fetch작성 후 await로 json을 가져옴
+    // const json = await response.json(); 이걸 안쓰려면 위의 함수처럼 작성하면 됨.
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  const onChangeUSD = (e) => setMyUSD(e.target.value);
-  console.log(cost);
-  console.log(myUSD);
-
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        //코인 불러오기 끝내면 로딩창 삭제
-        setLoading(false);
-      });
-    //일단 작성. coin을 생성하는 api를 실행, json을 통해 받아옴.
+    getMovies();
   }, []);
-  //[] 주시하려는 항목이 비어있으면 단 한번만 실행.
+  console.log(movies);
+
+  //오래된 방식
+  // useEffect(() => {
+  //   //fetch(``) API를 호출하는 함수. 백틱 안에 링크를 넣을 것
+  //   //한국 영화진흥위원회에서 제공하는 무료 API. 가입해서 키발급
+  //   //https://www.kobis.or.kr/kobisopenapi/homepg/apiservice/searchServiceInfo.do
+  //   fetch(
+  //     `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=71c925e2920963141d5e1dccdc3d6a19&targetDt=20230101 `
+  //   )
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setMovies(json.boxOfficeResult.dailyBoxOfficeList);
+  //       setLoading(false); //로딩이 끝나면 Loading문구를 안보기게 처리
+  //     });
+  // }, []);
+  //fetch().then((response) => response.json()) json을 리스폰한 후 리스폰 안에 json을 담는다
+  //.then((json)=> 내 useState set함수) 그 json을 useState안에 (배열위치)담아준다.
+  // *****!!!! 그런데 then()함수는 잘 안씀. async-await함수를 주로 사용.
+
   return (
     <div>
-      <h1>the Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <select onChange={onChange}>
-            <option>선택하세요</option>
-            {coins.map((coin) => (
-              <option key={coin.id} value={coin.quotes.USD.price}>
-                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-          <div>
-            바꿀 USD :{" "}
-            <input
-              onChange={onChangeUSD}
-              value={myUSD}
-              type="number"
-              placeholder="USD"
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
             />
-            <h3>얻을 수 있는 코인:{myUSD / cost}</h3>
-          </div>
+          ))}
         </div>
       )}
     </div>
---
+++ src/Movie.js
@@ -0,0 +1,33 @@
+//propTypes를 가져와서 적용시키기
+import propTypes from "prop-types";
+
+//부모한테서 props를 받아올것
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {
+          //genres가 없는 영화가 있는경우 에러가 나기 때문에
+          //장르가 있는 것만 불러오기 위해 &&을 적용
+          genres && genres.map((g) => <li key={g}>{g}</li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+//prop-types의 타입을 설정하기
+Movie.propTypes = {
+  coverImg: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  //장르는 배열이기 때문에arrayOf()안에 적어준다.
+  genres: propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+//클릭해서 페이지 넘어가는건 터미널에서 npm install react-router-dom 설치해줘야함.
+
+export default Movie;
